fa17aa750e63ebcd8d88a8f11b72c2fff099e155
fynaghkk <fynaghk@gmail.com>
2024-08-12 20:50:03 +0400

    ongoing

--- Data-Exploration.py
+++ Data-Exploration.py
@@ -1,6 +1,13 @@
 import pandas as pd 
 import numpy as np 
 
-moviesDataset = pd.read_csv('../../Datasets/movie.csv')
+moviesDataset = pd.read_csv('../Datasets/movie.csv')
 
-moviesDataset
+print(moviesDataset)
+
+#The number of empty cells is checked in the data
+moviesDataset.replace('(no genres listed)', pd.NA, inplace=True)
+empty_check = moviesDataset.iloc[:, 2].isnull().sum()
+print(empty_check)
+#We found 246 cells are empty
+
